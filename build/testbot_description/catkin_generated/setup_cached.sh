713492a7ef96ad913b1f1d6f40b003bcb9533226
shivamtrip <shivamtrip99@gmail.com>
2023-10-01 16:44:55 -0400

    fixed bugs

---
+++ build/testbot_description/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/shivam/3d-navigation-robot/devel/.private/testbot_description:$CMAKE_PREFIX_PATH"
+export PWD='/home/shivam/3d-navigation-robot/build/testbot_description'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/shivam/3d-navigation-robot/devel/.private/testbot_description/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/shivam/3d-navigation-robot/src/testbot_description:$ROS_PACKAGE_PATH"
